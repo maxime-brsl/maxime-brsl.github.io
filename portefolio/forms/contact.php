81d5c19cda4ee2b485172c93dc50f1dd00f9b972
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2022-03-28 18:47:48 +0200

    f

--- portefolio/forms/contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_POST['message'])) {
-    $retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
-    if($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-}
-?>
